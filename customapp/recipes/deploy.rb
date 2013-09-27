9472a8c6922340345b6bc3ac6577d2c94311c432
Jason Vasquez <jason.vasquez@developertown.com>
2013-09-26 22:19:26 -0400

    Toying with basic recipes to start a statsims worker (will generi-cize later)

---
+++ customapp/recipes/deploy.rb
@@ -0,0 +1,29 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'custom'
+    Chef::Log.debug("Skipping redis::configure as application #{application} as it is not a custom app")
+    next
+  end
+
+  include_recipe 'fetch'
+
+  bash "stopping_statsims_processes" do
+    user "deploy"
+    cwd "/tmp"
+    code <<-EOH
+      pgrep -f statsims
+      if [ "$?" == "0" ]; then
+        echo "Killing existing statsims engine instance"
+        pkill -f statsims
+      fi
+    EOH
+  end
+
+  bash "start_statsims_worker" do
+    user "deploy"
+    cwd "/tmp"
+    code <<-EOH
+      echo "$(date) Restarting" >> /tmp/worker-1.log
+      nohup ./statsims >> /tmp/worker-1.log 2> /tmp/worker-1.err < /dev/null &
+    EOH
+  end
+end
---
+++ customapp/recipes/fetch.rb
@@ -0,0 +1,4 @@
+remote_file "/tmp/statsims" do
+  source "http://dc.developertown.com/~jvasquez/statsims"
+  mode "0755"
+end
