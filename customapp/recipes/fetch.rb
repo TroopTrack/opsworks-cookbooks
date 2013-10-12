368686946bcc094124feb7bdd8a44513c43e7e45
Jason Vasquez <jason.vasquez@developertown.com>
2013-10-12 10:33:46 -0400

    Fix do block

--- customapp/recipes/fetch.rb
+++ customapp/recipes/fetch.rb
@@ -1,4 +1,4 @@
-node[:deploy].each do |application, deploy| do
+node[:deploy].each do |application, deploy|
   remote_file deploy[:application][:custom][:binary_dest] do
     source deploy[:application][:custom][:binary_url]
     mode "0755"
