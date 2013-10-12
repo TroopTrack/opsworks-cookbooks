2fae15afedcca97106f6eace9bfd3272f43ad4d4
Jason Vasquez <jason.vasquez@developertown.com>
2013-10-12 10:46:50 -0400

    Deubg

--- customapp/recipes/fetch.rb
+++ customapp/recipes/fetch.rb
@@ -1,9 +1,9 @@
 puts "DEBUG"
 puts node
 
-node[:deploy].each do |application, deploy|
-  remote_file node[:deploy][:application][:custom][:binary_dest] do
-    source node[:deploy][:application][:custom][:binary_url]
-    mode "0755"
-  end
-end
+#node[:deploy].each do |application, deploy|
+  #remote_file node[:deploy][:application][:custom][:binary_dest] do
+    #source node[:deploy][:application][:custom][:binary_url]
+    #mode "0755"
+  #end
+#end
