660817d932b8aacb9b5468bbe996bf1a6fadc5b2
Jason Vasquez <jason.vasquez@developertown.com>
2013-10-12 10:38:38 -0400

    Updating var name

--- customapp/recipes/fetch.rb
+++ customapp/recipes/fetch.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
-  remote_file deploy[:application][:custom][:binary_dest] do
-    source deploy[:application][:custom][:binary_url]
+  remote_file node[:deploy][:application][:custom][:binary_dest] do
+    source node[:deploy][:application][:custom][:binary_url]
     mode "0755"
   end
 end
