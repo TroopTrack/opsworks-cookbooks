d2af3e1e12ef3db3f1e01b89fada7f713b2dc692
Jason Vasquez <jason.vasquez@developertown.com>
2013-10-12 10:42:06 -0400

    Adding some logging

--- customapp/recipes/fetch.rb
+++ customapp/recipes/fetch.rb
@@ -1,3 +1,6 @@
+puts "DEBUG"
+puts node
+
 node[:deploy].each do |application, deploy|
   remote_file node[:deploy][:application][:custom][:binary_dest] do
     source node[:deploy][:application][:custom][:binary_url]
