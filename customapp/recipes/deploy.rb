cd0296cfa239bfddff4fc2bc0e5e9d23ba250eb4
Jason Vasquez <jason.vasquez@developertown.com>
2014-04-14 06:49:32 -0400

    Revert "Destroying this useless cookbook"
    This reverts commit efb07ead314b8a4bab258b850e4b936e68b19822.

---
+++ customapp/recipes/deploy.rb
@@ -0,0 +1,35 @@
+node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'other'
+    Chef::Log.debug("Skipping customapp::deploy as application #{application} as it is not a custom app")
+    next
+  end
+
+  puts "DEBUG:"
+  puts deploy
+
+  bash "stopping_statsims_processes" do
+    user "deploy"
+    cwd "/tmp"
+    code <<-EOH
+      pgrep -f statsims
+      if [ "$?" == "0" ]; then
+        echo "Killing existing statsims engine instance"
+        pkill -f statsims
+      fi
+    EOH
+  end
+
+  remote_file deploy[:custom][:binary_dest] do
+    source deploy[:custom][:binary_url]
+    mode "0755"
+  end
+
+  bash "start_statsims_worker" do
+    user "deploy"
+    cwd "/tmp"
+    code <<-EOH
+      echo "$(date) Restarting" >> #{deploy[:custom][:logstdout]}
+      #{deploy[:custom][:command]}
+    EOH
+  end
+end
