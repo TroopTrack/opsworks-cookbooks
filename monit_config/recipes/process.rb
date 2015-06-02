6290f8970ce6ebf5891b1f9251d9d7485809f50d
Tom Skibinski <linxsys-admin@linxsys.pl>
2015-06-02 15:22:18 +0200

    Additional if statement

--- monit_config/recipes/process.rb
+++ monit_config/recipes/process.rb
@@ -3,25 +3,27 @@
 # Recipe:: process
 #
 
-node[:deploy].each do |application, deploy|
-  Chef::Log.info("Configure monit service to track #{node[:monit_process][:process_name]} process...")
+if node[:opsworks][:instance][:hostname] == "backoffice-app1"
+  node[:deploy].each do |application, deploy|
+    Chef::Log.info("Configure monit service to track #{node[:monit_process][:process_name]} process...")
 
-  template "/etc/monit/conf.d/#{node[:monit_process][:process_name]}.monitrc" do
-    source "process.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    variables(
-      :process_info => node[:monit_process],
-      :app_user => deploy[:user],
-      :app_group => deploy[:group],
-      :app_path => '/srv/www/trooptrack_web',
-      :app_env => deploy[:rails_env]
-    )
-    action :create
+    template "/etc/monit/conf.d/#{node[:monit_process][:process_name]}.monitrc" do
+      source "process.erb"
+      owner "root"
+      group "root"
+      mode 0644
+      variables(
+        :process_info => node[:monit_process],
+        :app_user => deploy[:user],
+        :app_group => deploy[:group],
+        :app_path => '/srv/www/trooptrack_web',
+        :app_env => deploy[:rails_env]
+      )
+      action :create
+    end
   end
-end
 
-service 'monit' do
-  action :restart
+  service 'monit' do
+    action :restart
+  end
 end
--- monit_config/recipes/webgui.rb
+++ monit_config/recipes/webgui.rb
@@ -3,24 +3,26 @@
 # Recipe:: webgui
 #
 
-template "/etc/monit/monitrc" do
-  source "monitrc.erb"
-  mode 0600
-  owner "root"
-  group "root"
-  variables(
-    :username => node[:monit_config][:webgui][:username],
-    :password => node[:monit_config][:webgui][:password]
-  )
-  action :create
-end
+if node[:opsworks][:instance][:hostname] == "backoffice-app1"
+  template "/etc/monit/monitrc" do
+    source "monitrc.erb"
+    mode 0600
+    owner "root"
+    group "root"
+    variables(
+      :username => node[:monit_config][:webgui][:username],
+      :password => node[:monit_config][:webgui][:password]
+    )
+    action :create
+  end
 
-execute "build_monit_pem_certificate" do
-  command "cat /etc/nginx/ssl/#{node[:monit_process][:stack_firstly_added_domain]}.key > /etc/monit/monit.pem && cat /etc/nginx/ssl/#{node[:monit_process][:stack_firstly_added_domain]}.crt >> /etc/monit/monit.pem && chmod 600 /etc/monit/monit.pem"
-  user "root"
-  group "root"
-end
+  execute "build_monit_pem_certificate" do
+    command "cat /etc/nginx/ssl/#{node[:monit_process][:stack_firstly_added_domain]}.key > /etc/monit/monit.pem && cat /etc/nginx/ssl/#{node[:monit_process][:stack_firstly_added_domain]}.crt >> /etc/monit/monit.pem && chmod 600 /etc/monit/monit.pem"
+    user "root"
+    group "root"
+  end
 
-service 'monit' do
-  action :restart
+  service 'monit' do
+    action :restart
+  end
 end
