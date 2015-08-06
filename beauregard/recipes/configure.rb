f857ecaa2ce0d044b7bdf37608e060b6ed0e7e68
Tom Skibinski <linxsys-admin@linxsys.pl>
2015-08-06 10:56:00 +0200

    Run recipe only on desired nodes

--- beauregard/recipes/configure.rb
+++ beauregard/recipes/configure.rb
@@ -3,11 +3,13 @@
 # recipe:: configure
 #
 
-node[:deploy].each do |application, deploy|
-  Chef::Log.info("Configuring beauregard email processor for application #{application}")
+if node[:opsworks][:instance][:hostname] == "backoffice-app1" or node[:opsworks][:instance][:hostname] == "web-app1"
+  node[:deploy].each do |application, deploy|
+    Chef::Log.info("Configuring beauregard email processor for application #{application}")
 
-  template "/etc/init/beauregard.conf" do
-    source "beauregard.conf.erb"
-    mode "0644"
+    template "/etc/init/beauregard.conf" do
+      source "beauregard.conf.erb"
+      mode "0644"
+    end
   end
 end
--- beauregard/recipes/restart.rb
+++ beauregard/recipes/restart.rb
@@ -3,9 +3,11 @@
 # recipe:: restart
 #
 
-node[:deploy].each do |application, deploy|
-  service 'beauregard' do
-    action [:stop, :start]
-    provider Chef::Provider::Service::Upstart
+if node[:opsworks][:instance][:hostname] == "backoffice-app1" or node[:opsworks][:instance][:hostname] == "web-app1"
+  node[:deploy].each do |application, deploy|
+    service 'beauregard' do
+      action [:stop, :start]
+      provider Chef::Provider::Service::Upstart
+    end
   end
 end
