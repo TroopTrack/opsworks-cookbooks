05c65c213cbe38097017ba462e3dc1edf01647ba
rohandani-tudip <rohan.dani@tudip.com>
2022-03-11 23:55:32 +0530

    Add nodejs to staging environment Details:  - Updated the default file to get the nodejs 14 version.

--- nodesource_repo/recipes/default.rb
+++ nodesource_repo/recipes/default.rb
@@ -10,7 +10,7 @@ repo_path = "node_#{node["nodejs"]["version"]}"
 
 apt_repository "nodesource" do
   arch repo_arch
-  uri "https://deb.nodesource.com/#{repo_path}"
+  uri "https://deb.nodesource.com/setup_14.x"
   distribution node["lsb"]["codename"]
   components ["main"]
   key "https://deb.nodesource.com/gpgkey/nodesource.gpg.key"
